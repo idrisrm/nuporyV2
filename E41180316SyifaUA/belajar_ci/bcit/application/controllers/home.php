335fe1dbd9d3efabf5a55010b78328b38da4b58d
SyifaUA <syifa.urrosydah1810@gmail.com>
2020-02-27 08:35:53 +0700

    Update home.php

--- E41180316SyifaUA/belajar_ci/bcit/application/controllers/home.php
+++ E41180316SyifaUA/belajar_ci/bcit/application/controllers/home.php
@@ -5,9 +5,9 @@ public function index () {
     echo form_open ('/'); // membuka form
     echo form_label ('Nama : ')  . '<br>'; // membuat label
     echo form_input ('nama') . '<br>'; // membuat textbox
-    echo form_label ('Alamat : ') . '<br>' // membuat label
-    echo form_input ('alamat') . '<br>'; // membuat texbox
-    echo form_submit ('submit' , 'Kirim Data') // membuat tombol
+    echo form_label ('Alamat : ') . '<br>'; // membuat label
+    echo form_textarea ('alamat') . '<br>'; // membuat texbox
+    echo form_submit ('submit' , 'Kirim Data'); // membuat tombol
     echo form_close (); // menutup form
 
   }
