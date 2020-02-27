f7c36a0ffaa69c543ca51980308643d5563a2a60
SyifaUA <syifa.urrosydah1810@gmail.com>
2020-02-27 08:59:09 +0700

    Update home.php

--- E41180316SyifaUA/belajar_ci/bcit/application/controllers/home.php
+++ E41180316SyifaUA/belajar_ci/bcit/application/controllers/home.php
@@ -1,15 +1,10 @@
 <?php
 class Home extends CI_Controller {//mengextend CI_Controller
 public function index () {
-    $this->load->helper ("form"); //mengextends CI_Controller
-    echo form_open ('/'); // membuka form
-    echo form_label ('Nama : ')  . '<br>'; // membuat label
-    echo form_input ('nama') . '<br>'; // membuat textbox
-    echo form_label ('Alamat : ') . '<br>'; // membuat label
-    echo form_textarea ('alamat') . '<br>'; // membuat texbox
-    echo form_submit ('submit' , 'Kirim Data'); // membuat tombol
-    echo form_close (); // menutup form
-
+    $this->load->helper ("belajar"); //memanggil helper belajar
+    tampilkanTebal ("Politeknik Negeri Jember <br>");
+    tampilkanMiring ("Jurusan Teknologi Informasi <br>");
+    tampilkanBergaris ("2020 <br>");
   }
 }
 ?> 
