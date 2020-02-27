c2ede753bc8201eea06cd882305b954324dc6a29
SyifaUA <syifa.urrosydah1810@gmail.com>
2020-02-27 08:21:47 +0700

    Create HomeView.php

---
+++ E41180316SyifaUA/belajar_ci/bcit/application/views/HomeView.php
@@ -0,0 +1,9 @@
+<h1> Selamat datang di website Jurusan Teknologi Informasi</h1>
+<p>
+    Berikut ini mahasiswa bermasalah :
+    
+</p>
+
+Nama : <?php echo $data ["nama"] ; ?><br>
+Status : <?php echo $data ["status"] ; ?><br>
+Website : <?php echo $data ["website"] ; ?><br>
