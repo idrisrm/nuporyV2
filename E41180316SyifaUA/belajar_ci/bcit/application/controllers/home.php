8f34f4ad6f0ca2a6e9ffd518603112c8ce9b4455
SyifaUA <syifa.urrosydah1810@gmail.com>
2020-02-27 10:16:07 +0700

    Update home.php

--- E41180316SyifaUA/belajar_ci/bcit/application/controllers/home.php
+++ E41180316SyifaUA/belajar_ci/bcit/application/controllers/home.php
@@ -4,9 +4,10 @@ public function index () {
     //cek apakah method = post
     if ($this->input->method () == "post") {
     //tampilkan data
-    echo "nama : " . this->input->post ("nama") . '<br>';
-    echo "email : " . this->input->post ("email");
+    echo "nama : " . $this->input->post ("nama") . '<br>';
+    echo "email : " . $this->input->post ("email");
   }
   $this->load->view("HomeView");
+}
 }
 ?> 
