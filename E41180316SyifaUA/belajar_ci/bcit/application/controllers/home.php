dedb300e83dba33ffa84e11c3a312866c3649ce2
SyifaUA <syifa.urrosydah1810@gmail.com>
2020-03-02 13:12:58 +0700

    Update home.php

--- E41180316SyifaUA/belajar_ci/bcit/application/controllers/home.php
+++ E41180316SyifaUA/belajar_ci/bcit/application/controllers/home.php
@@ -1,30 +1,16 @@
 <?php
 class Home extends CI_Controller {//mengextend CI_Controller
 public function index () {
-  $error = "";
-  $data = "";
-    if ($this->input->method () == "post") {
-    //konfigurasi
-  $config ['upload_path'] = './gambar/';
-  $config ['allowed_types'] = 'gif|jpg|png';
-  $config ['max_size'] = 2000;
-  $config ['max_width'] = 1024;
-  $config ['max_height'] = 1768;
+  $this->load->library("sesion");
+  //set session
+  $this->session->set_userdata("nama" , "Politeknik");
+  //show session
+  echo 'Nama anda : ' . $this->session->userdata("nama")
+  echo '<br>Sesion di hapus <br>';
+  //hapus session nama
+  $this->session->unset_userdata("nama");
+  echo "Nama anda : " . $this->session->userdata("nama");
 
-  //panggil library
-  $this->load->library('upload', $config);
-
-  //cek apakah gagal upload
-  if (!$this->upload->do_upload('gambar')) {
-    $error = $this->upload->display_errors();
-  }else {//jika file berhasil di upload
-    $data = $this->upload->data();
-  }
-  }
-  $this->load->view("HomeView", array(
-    'error' => $error,
-    'data' => $data
-  ));
 }
 }
 ?> 
