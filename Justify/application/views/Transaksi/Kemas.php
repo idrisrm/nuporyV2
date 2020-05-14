6f1751667fd7764436a65df176e99bfe3015c72c
Moh. Saidul Musthofa <sayyidmusthofa89@gmail.com>
2020-05-15 01:14:22 +0700

    kemas

--- Justify/application/views/Transaksi/Kemas.php
+++ Justify/application/views/Transaksi/Kemas.php
@@ -32,7 +32,31 @@
                                         <td><?= $data["alamat"]?></td>
                                         <td><?= $data["total"]?></td>
                                         <td>
-                                            <button class="btn btn-primary">Kirim</button>
+                                            <!-- Button trigger modal -->
+                                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                            KIRIM
+                                            </button>
+
+                                            <!-- Modal -->
+                                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">Peringatan !</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Yakin Untuk Mengirim Bunga?
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">BATAL</button>
+                                                    <button type="button" class="btn btn-primary">KIRIM BUNGA</button>
+                                                </div>
+                                                </div>
+                                            </div>
+                                            </div>
                                         </td>
                                     </tr>
                                     <?php }?>
